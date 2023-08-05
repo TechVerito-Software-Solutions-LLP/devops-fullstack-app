8348224ed186d708753841af39065ec2428b4d3c
satheesh61 <satheeshkumar.u@thedatateam.com>
2023-08-05 18:58:27 +0530

    Added appspec and script file

---
+++ deployment_scripts/pull_and_run_docker_compose.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Retrieve the ECR repository URI and image tag
+API_ECR_REPOSITORY_URI="066747756520.dkr.ecr.ap-south-1.amazonaws.com/goapp-repo"
+UI_ECR_REPOSITORY_URI="066747756520.dkr.ecr.ap-south-1.amazonaws.com/reactapp-repo"
+IMAGE_TAG="latest"
+
+# Authenticate Docker with ECR
+$(aws ecr get-login --no-include-email --region ap-south-1)
+
+# Authenticate Docker with ECR
+$(aws ecr get-login --no-include-email --region ap-south-1)
+
+# Pull the Docker image from ECR
+docker pull "${API_ECR_REPOSITORY_URI}:${IMAGE_TAG}"
+docker pull "${UI_ECR_REPOSITORY_URI}:${IMAGE_TAG}"
+
+#Run the docker-compose
+ls -l
+docker-compose -f ./docker-compose.yml up -d
+
